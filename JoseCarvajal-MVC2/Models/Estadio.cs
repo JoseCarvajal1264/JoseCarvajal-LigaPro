f05ae59c2e62f9c36d4d1ea7d3dc7c8a3e799534
Jose Carvajal <joseluiscarvajal2004@gmail.com>
2024-11-11 15:32:43 -0500

    final

--- JoseCarvajal-MVC2/Models/Estadio.cs
+++ JoseCarvajal-MVC2/Models/Estadio.cs
@@ -1,7 +1,10 @@
-﻿namespace JoseCarvajal_MVC2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseCarvajal_MVC2.Models
 {
     public class Estadio
     {
+        [Key]
         public int Id { get; set; }
         public string Direccion { get; set; }
         public string Ciudad { get; set; }
--- JoseCarvajal-MVC2/Models/Jugador.cs
+++ JoseCarvajal-MVC2/Models/Jugador.cs
@@ -1,10 +1,14 @@
-﻿namespace JoseCarvajal_MVC2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseCarvajal_MVC2.Models
 {
     public class Jugador
     {
+        [Key]
         public int Id { get; set; } 
         public string Nombre { get; set; }
         public string Posicion { get; set; }
+        [Range(15, 45)]
         public int Edad { get; set; }
         public Equipo Equipo { get; set; }
 
